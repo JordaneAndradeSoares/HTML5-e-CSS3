0fff7ff24ed648f3b0535950efe7c54dcf6225d8
JordaneAndradeSoares <113549535+JordaneAndradeSoares@users.noreply.github.com>
2026-09-18 14:36:34 -0300

    Add files via upload
    atualização de js.js

--- js.js
+++ js.js
@@ -1,1 +1,13 @@
-console.log("ola")
+let formulario = document.getElementById("id_formulario");
+let campo_nome = document.getElementById("nome");
+
+formulario.addEventListener("submit", function(evento) {
+    evento.preventDefault();
+
+    if (campo_nome.value.trim() === "") {
+        alert("Você deve escrever algo antes de enviar");
+    } else {
+        alert("Obrigado, " + campo_nome.value + "! Seu nome não vai ser salvo");
+        campo_nome.value = "";
+    }
+});
